14694bee2d87955da83db1427b521d5f1cf5d08c
Hannlove19 <hannilopez19@gmail.com>
2025-03-01 22:56:28 -0600

    Create eje8.c

---
+++ eje8.c
@@ -0,0 +1,19 @@
+// Intercambio de Valores
+#include <stdio.h>
+
+void intercambiar(int *a, int *b) {
+    // Implementar intercambio aquí
+    int temp = *a;  
+    *a = *b;        
+    *b = temp;      
+}
+
+int main() {
+    int arr[] = {5, 10};
+    // Código para llamar a la función y mostrar los valores intercambiados
+     printf("Antes del intercambio: arr[0] = %d, arr[1] = %d\n", arr[0], arr[1]);
+     intercambiar(&arr[0], &arr[1]);
+     printf("Despues del intercambio: arr[0] = %d, arr[1] = %d\n", arr[0], arr[1]);
+
+    return 0;
+}
